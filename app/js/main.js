bbadecbf02fd28a6f2fe64b1f27c4aed1a3f4266
All4site <all4sitecomua@gmail.com>
2018-01-26 16:33:00 +0200

    work

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,14 @@
 
   $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+    $('.modal').modal({
+        inDuration: 400,
+        opacity: .9
+    });
      $('.carousel').carousel({
      	fullWidth: true,
      	indicators: true,
      	padding: 800,
-     	dist: 100
+     	dist: 100,
      });
   });
