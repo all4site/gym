b934d077d488df2e614389f4de42d7866a5dae45
All4site <all4sitecomua@gmail.com>
2018-01-29 17:12:42 +0200

    work

--- app/js/main.js
+++ app/js/main.js
@@ -11,4 +11,14 @@
      	padding: 800,
      	dist: 100,
      });
+     $(".button-collapse").sideNav();
   });
+
+  $(document).ready(function() {
+    $('a[data-target="menu"]').click(function() {
+        var target = $(this).attr('href');
+        $('a').removeClass('active');
+        $(this).addClass('active');
+        $('html, body').animate({ scrollTop: $(target).offset().top}, 700);
+    });
+});
